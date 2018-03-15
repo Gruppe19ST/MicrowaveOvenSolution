b9feff7083e7514335feb81a5332389087724462
Anni Lykke Wilhelmsen <201509504@post.au.dk>
2018-03-15 11:11:02 +0100

    IT1

--- Microwave.Test.Integration/IT1_Userinterface.cs
+++ Microwave.Test.Integration/IT1_Userinterface.cs
@@ -1,12 +1,40 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using NUnit.Framework;
+using NSubstitute;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
 
 namespace Microwave.Test.Integration
 {
+    [TestFixture]
     public class IT1_Userinterface
     {
+        // Drivers
+        private Door _driverDoor;
+        private Button _driverPowerButton;
+        private Button _driverTimeButton;
+        private Button _driverStartCancelButton;
+        // Unit under test
+        private UserInterface _uut;
+        // Stubs/mocks
+        private ICookController _cookController;
+        private IDisplay _display;
+        private ILight _light;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Drivers
+            _driverDoor = new Door();
+            _driverPowerButton = new Button();
+            _driverTimeButton = new Button();
+            _driverStartCancelButton = new Button();
+            // Unit under test
+            _uut = new UserInterface(_driverPowerButton,_driverTimeButton,_driverStartCancelButton,_driverDoor,_display,_light,_cookController);
+            // Stubs/mocks
+            _cookController = Substitute.For<ICookController>();
+            _display = Substitute.For<IDisplay>();
+            _light = Substitute.For<ILight>();
+        }
     }
 }
