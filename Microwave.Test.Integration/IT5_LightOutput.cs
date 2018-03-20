2b44167054ae65676fd9d3afe17137e8766d7a50
Anni Lykke Wilhelmsen <201509504@post.au.dk>
2018-03-20 11:47:44 +0100

    IT5 og IT7

---
+++ Microwave.Test.Integration/IT5_LightOutput.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    class IT5_LightOutput
+    {
+        // Drivers
+        private Door _driverDoor;
+        private Button _driverPowerButton;
+        private Button _driverTimeButton;
+        private Button _driverStartCancelButton;
+
+        // Stubs/mocks
+        private IDisplay _display;
+        private ITimer _timer;
+        private IPowerTube _powerTube;
+
+        // Unit under test
+        private Output _output;
+
+        // Included
+        private UserInterface _userInterface;
+        private CookController _cookController;
+        private Light _light;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Drivers
+            _driverDoor = new Door();
+            _driverPowerButton = new Button();
+            _driverTimeButton = new Button();
+            _driverStartCancelButton = new Button();
+
+            // Stubs/mocks
+            _display = Substitute.For<IDisplay>();
+            _timer = Substitute.For<ITimer>();
+            _powerTube = Substitute.For<IPowerTube>();
+
+            // Unit under test
+            _output = new Output();
+
+            // Included
+            _light = new Light(_output);
+            _cookController = new CookController(_timer, _display, _powerTube);
+            _userInterface = new UserInterface(_driverPowerButton, _driverTimeButton, _driverStartCancelButton,
+                _driverDoor, _display, _light, _cookController);
+            _cookController.UI = _userInterface;
+        }
+
+        #region Light-Output.Integration
+
+        [Test]
+        public void OpenDoor_LightTurnOn_LogLineCalled()
+        {
+            _driverDoor.Open();
+            _output.OutputLine("Light is turned on");
+        }
+
+        [Test]
+        public void OpenDoor_LightTurnOff_LogLineCalled()
+        {
+            _driverDoor.Close();
+            _output.OutputLine("Light is turned off");
+        }
+
+        [Test]
+        public void CookingIsDone_LightTurnOff_LogLineCalled()
+        {
+            _driverPowerButton.Press();
+            _driverTimeButton.Press();
+            _driverStartCancelButton.Press();
+
+            _output.OutputLine("Light is turned off");
+        }
+
+        #endregion
+    }
+}
---
+++ Microwave.Test.Integration/IT7_PowerTubeOutput.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace Microwave.Test.Integration
+{
+    class IT7_PowerTubeOutput
+    {
+        // Drivers
+        private Door _driverDoor;
+        private Button _driverPowerButton;
+        private Button _driverTimeButton;
+        private Button _driverStartCancelButton;
+
+        // Stubs/mocks
+        private ILight _light;
+        private ITimer _timer;
+        private IDisplay _display;
+
+        // Unit under test
+        private Output _output;
+
+        // Included
+        private UserInterface _userInterface;
+        private CookController _cookController;
+        private PowerTube _powerTube;
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Drivers
+            _driverDoor = new Door();
+            _driverPowerButton = new Button();
+            _driverTimeButton = new Button();
+            _driverStartCancelButton = new Button();
+
+            // Stubs/mocks
+            _light = Substitute.For<ILight>();
+            _timer = Substitute.For<ITimer>();
+            _display = Substitute.For<IDisplay>();
+
+            // Unit under test
+            _output = new Output();
+
+            // Included
+            _powerTube = new PowerTube(_output);
+            _cookController = new CookController(_timer, _display, _powerTube);
+            _userInterface = new UserInterface(_driverPowerButton, _driverTimeButton, _driverStartCancelButton, _driverDoor, _display, _light, _cookController);
+            _cookController.UI = _userInterface;
+
+        }
+
+        #region PowerTube-Output.Integration
+
+        [Test]
+        public void OnStartCancelPressed_PowerTubeTurnOn_LogLineCalled()
+        {
+            _driverPowerButton.Press();
+            _driverPowerButton.Press();
+            _driverTimeButton.Press();
+            _driverStartCancelButton.Press();
+            _output.OutputLine($"PowerTube works with {100} %");
+        }
+
+        [Test]
+        public void OpenDoor_LightTurnOff_LogLineCalled()
+        {
+            _driverDoor.Close();
+            _output.OutputLine("Light is turned off");
+        }
+
+        [Test]
+        public void CookingIsDone_LightTurnOff_LogLineCalled()
+        {
+            _driverPowerButton.Press();
+            _driverTimeButton.Press();
+            _driverStartCancelButton.Press();
+
+            _output.OutputLine("Light is turned off");
+        }
+
+        #endregion
+    }
+}
