e5972fc0725d0f4043791e39eb75a71a95bc863f
Anni Lykke Wilhelmsen <201509504@post.au.dk>
2018-03-21 20:28:37 +0100

    Unit test Timer

--- Microwave.Test.Unit/TimerTest.cs
+++ Microwave.Test.Unit/TimerTest.cs
@@ -21,10 +21,13 @@ namespace Microwave.Test.Unit
             ManualResetEvent pause = new ManualResetEvent(false);
 
             uut.TimerTick += (sender, args) => pause.Set();
-            uut.Start(2000);
+            // Ændret da start tager sekunder frem for millisekunder
+            // 10 seconds
+            uut.Start(60/6);
 
             // wait for a tick, but no longer
-            Assert.That(pause.WaitOne(1100));
+            // Lidt over 10 sekunder
+            Assert.That(pause.WaitOne(10100));
 
             uut.Stop();
         }
@@ -50,14 +53,13 @@ namespace Microwave.Test.Unit
             ManualResetEvent pause = new ManualResetEvent(false);
 
             uut.Expired += (sender, args) => pause.Set();
-            // Ændret, da vi arbejder i sekunder i stedet for millisekunder
-            int seconds = 60;
-            int minutes = 1 * seconds;
-            int wait = (seconds * 1000) + 1000;
-            uut.Start(minutes);
+            // Ændret da start tager sekunder frem for millisekunder
+            // 10 seconds
+            uut.Start(60/6);
 
             // wait for expiration, but not much longer, should come
-            Assert.That(pause.WaitOne(wait));
+            // 11 second
+            Assert.That(pause.WaitOne(11000));
 
             uut.Stop();
         }
@@ -68,10 +70,13 @@ namespace Microwave.Test.Unit
             ManualResetEvent pause = new ManualResetEvent(false);
 
             uut.Expired += (sender, args) => pause.Set();
-            uut.Start(2000);
+            // Ændret da start tager sekunder frem for millisekunder
+            // 10 seconds
+            uut.Start(60/6);
 
             // wait shorter than expiration, shouldn't come
-            Assert.That(!pause.WaitOne(1900));
+            // 8 sekunder
+            Assert.That(!pause.WaitOne(8000));
 
             uut.Stop();
         }
